8f51ecc5b5dae4c0e9c700266fb184636fd97c27
shubham88fru <shubham88fru@yahoo.com>
2024-06-09 20:45:02 -0500

    documentatin

--- account/src/main/java/org/learning/smsvc/account/AccountApplication.java
+++ account/src/main/java/org/learning/smsvc/account/AccountApplication.java
@@ -1,11 +1,37 @@
 package org.learning.smsvc.account;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition (
+		info = @Info(
+				title = "Accounts microservice REST API documentation",
+				description = "EazyBank Accounts microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact (
+						name = "Shubham Singh",
+						email = "tempmail@mail.com",
+						url = "https://www.iife.me"
+				),
+				license = @License (
+						name = "Apache 2.0",
+						url="https://www.iife.me"
+				)
+		),
+		externalDocs = @ExternalDocumentation (
+				description = "EazyBank Accounts microservice REST API Documentation",
+				url = "https://iife.me"
+		)
+
+)
 public class AccountApplication {
 
 	public static void main(String[] args) {
